49cee67a1e02aa2534379c2050eea4654b8dce7a
Findys <findys1024@gmail.com>
2015-12-10 14:27:11 +0800

    CoreData数据存读完成

--- Gank/IAViewController.swift
+++ Gank/IAViewController.swift
@@ -13,15 +13,16 @@ class IAViewController: UIViewController,UITableViewDataSource,UITableViewDelega
     var URL = String()
     var dataSource = NSMutableArray()
     var ifandroid = Bool()
+    var localData = NSArray()
     var context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
-    var i = 1
+    var i = 2
     @IBOutlet weak var newsTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         if ifandroid{
-            URL = "http://gank.avosapps.com/api/data/android/10/1"
+            URL = "http://gank.avosapps.com/api/data/android/10/"
         }else{
-         URL = "http://gank.avosapps.com/api/data/iOS/10/1"
+            URL = "http://gank.avosapps.com/api/data/iOS/10/"
         }
         loadData(URL)
         newsTableView.mj_header = MJRefreshNormalHeader(refreshingBlock: { () -> Void in
@@ -30,43 +31,47 @@ class IAViewController: UIViewController,UITableViewDataSource,UITableViewDelega
         newsTableView.mj_footer = MJRefreshAutoNormalFooter(refreshingBlock: { () -> Void in
             self.loadMoreData()
         })
-        // Do any additional setup after loading the view.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(animated: Bool) {
-//        let changHeight = newsTableView.frame.height-30
-//        let width = newsTableView.frame.width
-//        UIView.beginAnimations(nil, context: nil)
-//        UIView.setAnimationDuration(2.0)
-//        newsTableView.frame = CGRect(x: 200, y: 100, width: 100, height: 300)
-//        UIView.commitAnimations()
-//            
-//        print("asd")
+        //        let changHeight = newsTableView.frame.height-30
+        //        let width = newsTableView.frame.width
+        //        UIView.beginAnimations(nil, context: nil)
+        //        UIView.setAnimationDuration(2.0)
+        //        newsTableView.frame = CGRect(x: 200, y: 100, width: 100, height: 300)
+        //        UIView.commitAnimations()
+        //
+        //        print("asd")
     }
     
     
     
-//    首次加载数据
+    //    首次加载数据
     func loadData(URL:String){
+        let loadUrl = URL + "1"
         let afmanager = AFHTTPRequestOperationManager()
-        afmanager.GET(URL, parameters: nil, success: { (AFHTTPRequestOperation, resp:AnyObject) -> Void in
+        afmanager.GET(loadUrl, parameters: nil, success: { (AFHTTPRequestOperation, resp:AnyObject) -> Void in
             let results = resp.objectForKey("results")! as! NSArray
             for each in results{
                 let item = NewsItem()
                 let row = NSEntityDescription.insertNewObjectForEntityForName("IOSNews", inManagedObjectContext: self.context)
-                item.author = each.objectForKey("who")! as! NSString
-                row.setValue(item.author, forKey: "author")
-                item.title = each.objectForKey("desc")! as! NSString
-                row.setValue(item.title, forKey: "title")
-                item.url = each.objectForKey("url")! as! NSString
-                row.setValue(item.url, forKey: "url")
-                item.time = each.objectForKey("publishedAt") as! NSString
-                row.setValue(item.time, forKey: "time")
+                item.author = each.objectForKey("who")! as! String
+                item.title = each.objectForKey("desc")! as! String
+                item.url = each.objectForKey("url")! as! String
+                item.time = each.objectForKey("publishedAt") as! String
+                
+                if self.dataSource.count < 10{
+                    row.setValue(item.title, forKey: "title")
+                    row.setValue(item.time, forKey: "time")
+                    row.setValue(item.author, forKey: "author")
+                }
+                
                 try! self.context.save()
                 self.dataSource.addObject(item)
             }
@@ -77,36 +82,77 @@ class IAViewController: UIViewController,UITableViewDataSource,UITableViewDelega
                 
             })
             }) { (AFHTTPRequestOperation, error:NSError) -> Void in
-                print(error)
+                let f = NSFetchRequest(entityName: "IOSNews")
+                self.localData = try! self.context.executeFetchRequest(f)
+                for i in 0...9{
+                    let item = NewsItem()
+                    item.author = self.localData[i].valueForKey("author")! as! String
+                    item.time = self.localData[i].valueForKey("time")! as! String
+                    item.title = self.localData[i].valueForKey("title")! as! String
+                    self.dataSource.addObject(item)
+                }
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    self.newsTableView.reloadData()
+                    self.newsTableView.mj_header.endRefreshing()
+                })
         }
     }
     
+    //    加载更多数据
     func loadMoreData(){
         URL = "http://gank.avosapps.com/api/data/iOS/10/"+String(i++)
-        loadData(URL)
+        let afmanager = AFHTTPRequestOperationManager()
+        afmanager.GET(URL, parameters: nil, success: { (AFHTTPRequestOperation, resp:AnyObject) -> Void in
+            let results = resp.objectForKey("results")! as! NSArray
+            for each in results{
+                let item = NewsItem()
+                let row = NSEntityDescription.insertNewObjectForEntityForName("IOSNews", inManagedObjectContext: self.context)
+                item.author = each.objectForKey("who")! as! String
+                item.title = each.objectForKey("desc")! as! String
+                item.url = each.objectForKey("url")! as! String
+                item.time = each.objectForKey("publishedAt") as! String
+                
+                if self.dataSource.count < 10{
+                    row.setValue(item.title, forKey: "title")
+                    row.setValue(item.time, forKey: "time")
+                    row.setValue(item.author, forKey: "author")
+                }
+                
+                try! self.context.save()
+                self.dataSource.addObject(item)
+            }
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.newsTableView.reloadData()
+                self.newsTableView.mj_header.endRefreshing()
+                self.newsTableView.mj_footer.endRefreshing()
+                
+            })
+            }) { (AFHTTPRequestOperation, error:NSError) -> Void in
+                self.newsTableView.mj_footer.endRefreshing()
+        }
+        
     }
     
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        print(dataSource.count)
         return dataSource.count
-
+        
     }
     
-//    加载每个cell的数据
+    //    加载每个cell的数据
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
         let title = cell.viewWithTag(1) as! UILabel
         let author = cell.viewWithTag(2) as! UILabel
         
         let item = dataSource[indexPath.row] as! NewsItem
-      
+        
         title.text = item.title as String
         author.text = item.author as String
         return cell
     }
     
-//    每个Cell的点击事件
+    //    每个Cell的点击事件
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
         let myStoryboard = UIStoryboard.init(name: "Main", bundle: nil)
         let webView = myStoryboard.instantiateViewControllerWithIdentifier("webView") as! WebViewController
@@ -118,15 +164,15 @@ class IAViewController: UIViewController,UITableViewDataSource,UITableViewDelega
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
